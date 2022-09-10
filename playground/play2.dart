58c80959159bcf87c801b2891655bb167c709d2c
a <ruan65@gmail.com>
2022-09-10 18:18:42 +0300

    threesum

--- playground/play2.dart
+++ playground/play2.dart
@@ -1,5 +1,12 @@
 void main() {
   print('777'.parseInt());
+  final s1 = <int>{1};
+  final s2 = <int>{1};
+  print(s1 == s2);
+
+  final m1 = {1:1};
+  final m2 = {1:1};
+  print(m1 == m2);
 }
 
 extension NumberParsing on String {
