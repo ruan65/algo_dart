ea32f7cf71d350f3486eb29f188ceb359d97ac3b
a <ruan65@gmail.com>
2022-09-21 00:36:46 +0300

    house_robber

--- 198_house_robber.dart
+++ 198_house_robber.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-int rob(List<int> nums) {
+int rob_(List<int> nums) {
   if (nums.length == 1) return nums.first;
   if (nums.length == 2) {
     return max(nums.first, nums.last);
@@ -8,6 +8,20 @@ int rob(List<int> nums) {
   return max(nums.first + rob(nums.sublist(2)), rob(nums.sublist(1)));
 }
 
+int rob(List<int> nums) {
+  if (nums.length == 1) return nums.first;
+  int pp = nums.first; // previous-previous
+  int p = nums[1]; // previous
+  for (int n in nums.sublist(2)) {
+    final tmp = p;
+    p = n + pp;
+    pp = max(tmp, pp);
+  }
+  return max(p, pp);
+}
+
 void main() {
-  print(rob([1, 2, 3, 1, 5, 4, 3, 5, 1]));
+  final lst = [1, 2, 3, 1, 5, 4, 3, 5, 1];
+  print(rob_(lst));
+  print(rob(lst));
 }
